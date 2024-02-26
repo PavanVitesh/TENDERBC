88ff2030c55d756ccf318e8aa56bf47dddc0fe51
pavan <kuncham.vitesh.67@gmail.com>
2024-02-26 16:56:19 +0530

    Develped login and logout functionalities

--- TENDERBC_Project/TENDERBC_App/urls.py
+++ TENDERBC_Project/TENDERBC_App/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as ad
 urlpatterns = [
     path('',views.Home,name='hm'),
     path('register/',views.Register,name='rg'),
-    # path('login/',ad.LoginView.as_view(template_name="html/login.html"),name='ln'),
-    # path('logout/',ad.LogoutView.as_view(template_name="html/logout.html"),name="lo"),
+    path('login/',ad.LoginView.as_view(template_name="html/login.html"),name='ln'),
+    path('logout/',ad.LogoutView.as_view(template_name="html/logout.html"),name="lo"),
 ]
