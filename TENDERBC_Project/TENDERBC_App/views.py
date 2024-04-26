a673ccc6154ea0790044ab21425cbc67c43be26b
PavanVitesh <kuncham.vitesh.67@gmail.com>
2024-04-26 17:39:04 +0530

    View Tender UI updated

--- TENDERBC_Project/TENDERBC_App/views.py
+++ TENDERBC_Project/TENDERBC_App/views.py
@@ -13,7 +13,6 @@ from django.core.mail import send_mail
 # pattern: send_mail(subject,mail body, from mail -> app mail id given in settings.py at last check (settings.EMAIL_HOST_USER),list of mail ids to which mail has to be sent)
 # example: # send_mail("Welcome to TENDERBC","Registartion Successful",settings.EMAIL_HOST_USER,[request.user.email])
 
-
 def update_status():
     tends = Tender.objects.all()
     for i in tends:
@@ -140,12 +139,12 @@ def View_Tender(request,x):
                     bidsubmission.document = outputpath
                     bidsubmission.save()
                     messages.success(request, 'Your bid has been submitted successfully, and the Secret Key has been downloaded. Please keep it safe for later use.')
-                    mail_service("Bid submission successfull!", "You have submitted you bid for following Tender: " + "\nTitle: "+ details.title + "\nDescription: " + detais.description + "\nExpiry: " + details.end_date_time, request.user.id)
+                    mail_service("Bid submission successfull!", "You have submitted you bid for following Tender: " + "\nTitle: "+ details.title + "\nDescription: " + detais.description + "\nExpiry: " + str(details.end_date_time), request.user.id)
                     messages.success(request, "")
                 except:
                     bidsubmission.delete()
                     messages.error(request, 'Error while saving to blockchain')
-                    mail_service("Bid submission failed!", "Your bid submission for following Tender: " + "\nTitle: "+ details.title + "\nDescription: " + detais.description + "\nExpiry: " + details.end_date_time + "\nis Failed! please resubmit your bid", request.user.id)
+                    mail_service("Bid submission failed!", "Your bid submission for following Tender: " + "\nTitle: "+ details.title + "\nDescription: " + details.description + "\nExpiry: " + str(details.end_date_time) + "\nis Failed! please resubmit your bid", request.user.id)
         if details.Status == 'Key Submission':
             secret_key = request.POST.get('secret_key')
             if not secret_key:
@@ -202,15 +201,15 @@ def Accept_Bid(request,x):
     all_bids = Bid.objects.filter(tender_id=bid_details.tender_id)
     for i in all_bids:
         if i.Status == "Ignored":
-            mail_service("Bid Ignored!", "Bid submitted to following tender is IGNORED!!!" + "\nTitle: "+ tender_details.title + "\nDescription: " + tender_detais.description + "\nExpiry: " + tender_details.end_date_time, i.bidder_id)
+            mail_service("Bid Ignored!", "Bid submitted to following tender is IGNORED!!!" + "\nTitle: "+ tender_details.title + "\nDescription: " + tender_detais.description + "\nExpiry: " + str(tender_details.end_date_time), i.bidder_id)
         else:
             i.Status = "Rejected"
             i.save()
             if i.bidder_id != bid_details.bidder_id:
-                mail_service("Bid Rejected!", "Bid submitted to following tender is REJECTED!!!" + "\nTitle: "+ tender_details.title + "\nDescription: " + tender_detais.description + "\nExpiry: " + tender_details.end_date_time, i.bidder_id)
+                mail_service("Bid Rejected!", "Bid submitted to following tender is REJECTED!!!" + "\nTitle: "+ tender_details.title + "\nDescription: " + tender_detais.description + "\nExpiry: " + str(tender_details.end_date_time), i.bidder_id)
     bid_details.Status = "Accepted"
     bid_details.save()
-    mail_service("Bid Accepted!", "Bid submitted to following tender is ACCEPTED!!!" + "\nTitle: "+ tender_details.title + "\nDescription: " + tender_detais.description + "\nExpiry: " + tender_details.end_date_time, bid_details.bidder_id)
+    mail_service("Bid Accepted!", "Bid submitted to following tender is ACCEPTED!!!" + "\nTitle: "+ tender_details.title + "\nDescription: " + tender_detais.description + "\nExpiry: " + str(tender_details.end_date_time), bid_details.bidder_id)
     tender_details.Status = "Granted"
     bidder_name = User.objects.get(id=bid_details.bidder_id).username
     messages.success(request, bidder_name + ' has been granted the tender')
